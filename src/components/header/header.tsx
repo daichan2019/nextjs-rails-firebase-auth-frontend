b191c09281775cb2e6125bffded3d3530e306f31
daichi_sato <daichiexperience@gmail.com>
2023-01-12 14:41:28 +0900

    fix filename

---
+++ src/components/header/header.tsx
@@ -0,0 +1,16 @@
+import type { FC } from 'react';
+
+import { useUserState } from '@/atoms/user';
+
+export const Header: FC = () => {
+  const currentUser = useUserState();
+
+  return (
+    <header className='shadow-md'>
+      <div className='flex justify-between py-6 px-4'>
+        <h1 className='text-xl font-bold'>Next.js + Rails Task App</h1>
+        <p>{currentUser?.email}</p>
+      </div>
+    </header>
+  );
+};
